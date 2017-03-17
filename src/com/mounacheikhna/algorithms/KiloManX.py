4edbbc0ac19aea490b027615a91b9304ba713bfe
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-03-17 07:21:15 +0100

    Improving the solution.

--- src/com/mounacheikhna/algorithms/KiloManX.py
+++ src/com/mounacheikhna/algorithms/KiloManX.py
@@ -13,6 +13,12 @@ class Node(object):
 
 class KiloManX:
     def leastShots(self, damageChart, bossHealth):
+        """
+        :param damageChart: in it the ith element containing N one-digit numbers ('0'-'9'), detailing the damage done
+                            to bosses 0, 1, 2, ..., N-1 by the weapon obtained from boss i
+        :param bossHealth: in it the ith element representing the amount of health that boss i has.
+        :return: an int which is the least number of shots that need to be fired to defeat all of the bosses.
+        """
         pq = []
         # represent each weapon as a bit in an integer, we will have to store a maximum of 32,768 values
         # (2^15, as there is a maximum of 15 weapons). So we can make our visited array simply be an
@@ -64,13 +70,15 @@ class KiloManX:
         return -1
 
 
-#damageChart = ["198573618294842", "159819849819205", "698849290010992", "000000000000000", "139581938009384",
-#               "158919111891911", "182731827381787", "135788359198718", "187587819218927", "185783759199192",
-#               "857819038188122", "897387187472737", "159938981818247", "128974182773177", "135885818282838"]
-#bossHealth = [157, 1984, 577, 3001, 2003, 2984, 5988, 190003, 9000, 102930, 5938, 1000000, 1000000, 5892, 38]
+# damageChart = ["1 98573618294842", "159819849819205", "698849290010992", "000000000000000", "139581938009384",
+#                "158919111891911", "182731827381787", "135788359198718", "187587819218927", "185783759199192",
+#                "857819038188122", "897387187472737", "159938981818247", "128974182773177", "135885818282838"]
+# bossHealth = [157, 1984, 577, 3001, 2003, 2984, 5988, 190003, 9000, 102930, 5938, 1000000, 1000000, 5892, 38]
 
-# damageChart = ["070", "500", "140"]
-# bossHealth = [150, 150, 150]
+damageChart = ["070", "500", "140"]
+bossHealth = [150, 150, 150]
+a = KiloManX()
+print(a.leastShots(damageChart, bossHealth))
 
 damageChart = ["1542", "7935", "1139", "8882"]
 bossHealth = [150, 150, 150, 150]
