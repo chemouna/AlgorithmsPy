c5807311c9555ade0876596541f9055de9ff9279
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-03-25 12:28:55 +0100

    Analysing calls to lcs.3

--- src/com/mounacheikhna/algorithms/dynamicprogramming/lcs.py
+++ src/com/mounacheikhna/algorithms/dynamicprogramming/lcs.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+
 def lcs(xs, ys):
     """
     Return a longest common subsequence of xs and ys.
@@ -13,5 +16,33 @@ def lcs(xs, ys):
         return []
 
 
-print(lcs("HUMAN", "CHIMPANZEE"))
+concat = ''.join
 
+
+def count_lcs_calls(lcs):
+    """
+    Return a pair (lcs, calls)
+
+    Where:
+    lcs - a wrapped version of lcs, which counts up calls
+    calls - a dict mapping arg pairs to the number of times lcs
+    has been called with these args.
+    """
+    calls = defaultdict(int)
+
+    def wrapped(xs, ys):
+        calls[(concat(xs), concat(ys))] += 1
+        return lcs(xs, ys)
+
+    return wrapped, calls
+
+
+lcs, calls = count_lcs_calls(lcs)
+
+# print(lcs("HUMAN", "CHIMPANZEE"))
+
+# print(lcs('MAN', 'PIG'))
+# print(*sorted((v, k) for k, v in calls.items()), sep='\n')
+
+print(lcs('CHIMP', 'CHIMP'))
+print(*sorted((v, k) for k, v in calls.items()), sep='\n')
