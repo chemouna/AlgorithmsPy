3369c2526c7f056390bfc0ae7854031765f913ff
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-03-26 15:20:10 +0200

    Display result of matrix chain multiplication.

--- src/com/mounacheikhna/algorithms/dynamicprogramming/matrix_chain_multiplication.py
+++ src/com/mounacheikhna/algorithms/dynamicprogramming/matrix_chain_multiplication.py
@@ -35,11 +35,13 @@ def matrix(m, n):
 # example: [1, 2, 3] represents a 1 x 2 and 2 x 3 matrix, in that order
 # @return: the minimum number of operations it would take to multiply the
 # sequence of matrices most efficiently
+# to understand this more use the figure representing the top matrix in Intro to Algorithms books
 def matrix_chain_order(p):
     n = len(p) - 1  # number of matrices in input
-    costs = [[0 for _ in range(n)] for _ in range(n)]  # an empty nxn cost matrix
+    costs = matrix(n, n)  # an empty nxn cost matrix
+    s = matrix(n, n)
     # length: number of matrices to consider
-    for length in range(2, n + 1):  # start from 2 because we dont need to calculate multiplication for one matrix
+    for length in range(2, n + 1):  # start from 2 because we don't need to calculate multiplication for one matrix
         # since its 0 (default value)
         # i: the 0-index of the matrix
         for i in range(n - length + 1):
@@ -51,7 +53,22 @@ def matrix_chain_order(p):
             # the right edge (which is k + 1).
             for k in range(i, j):
                 possible_cost = costs[i][k] + costs[k + 1][j] + p[i] * p[k + 1] * p[j + 1]
-                costs[i][j] = min(costs[i][j], possible_cost)
-    return costs[0][n - 1]
+                if possible_cost < costs[i][j]:
+                    costs[i][j] = possible_cost
+                    s[i][j] = k
+    return costs, s
 
-print(matrix_chain_order(arr))
+
+def print_matrix_chain(S, i, j):
+    if i == j:
+        return " A_" + str(i) + " "
+    else:
+        s = "("
+        k = S[i][j]
+        s += print_matrix_chain(S, i, k)
+        s += print_matrix_chain(S, k + 1, j)
+        s += ")"
+        return s
+
+t, s = matrix_chain_order(arr)
+print(print_matrix_chain(s, 1, 3))
