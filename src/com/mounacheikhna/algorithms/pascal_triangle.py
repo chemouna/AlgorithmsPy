4dbfd0b788a037288358480edfb8b5de0424571a
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-05-02 03:50:27 +0200

    Solution with memoization.

--- src/com/mounacheikhna/algorithms/pascal_triangle.py
+++ src/com/mounacheikhna/algorithms/pascal_triangle.py
@@ -1,3 +1,6 @@
+import collections
+
+
 # recursive solution
 def value(row, index):
     if index < 0 or index > row:
@@ -13,3 +16,21 @@ def row(n):
 
 for i in range(10):
     print(row(i))
+
+# With memoization
+values = collections.defaultdict(dict)
+
+
+def value_mem(row, index):
+    result = values[row].get(index)
+    if result is not None:
+        return result
+    if index < 0 or index > row:
+        return 0
+    if index == 0 or index == row:
+        return 1
+    result = value(row - 1, index) + value(row - 1, index - 1)
+    values[row][index] = result
+    return result
+
+
