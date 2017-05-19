1225ad317a2b52e40e77662f72c1047700260750
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-05-19 19:57:24 +0200

    BFS.

--- src/com/mounacheikhna/algorithms/graph/bfs.py
+++ src/com/mounacheikhna/algorithms/graph/bfs.py
@@ -1,1 +1,18 @@
+def bfs(graph, start):
+    visited, queue = set(), [start]
+    while queue:
+        vertex = queue.pop(0)
+        if vertex not in visited:
+            visited.add(vertex)
+            queue.extend(graph[vertex] - visited)
+    return visited
+
+g = {'A': {'B', 'C'},
+     'B': {'A', 'D', 'E'},
+     'C': {'A', 'F'},
+     'D': {'B'},
+     'E': {'B', 'F'},
+     'F': {'C', 'E'}}
+
+print(bfs(g, 'A'))
 
