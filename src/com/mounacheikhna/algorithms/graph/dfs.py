453a3c6fbace68950d75bb2c09f9efad51f01676
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-05-19 16:34:33 +0200

    DFS.

---
+++ src/com/mounacheikhna/algorithms/graph/dfs.py
@@ -0,0 +1,18 @@
+
+def dfs(graph, start):
+    visited, stack = set(), [start]
+    while stack:
+        vertex = stack.pop()
+        if vertex not in visited:
+            visited.add(vertex)
+            stack.extend(graph[vertex] - visited)
+    return visited
+
+
+graph = {'A': {'B', 'C'},
+         'B': {'A', 'D', 'E'},
+         'C': {'A', 'F'},
+         'D': {'B'},
+         'E': {'B', 'F'},
+         'F': {'C', 'E'}}
+print(dfs(graph, 'A'))
