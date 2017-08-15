9126887728b66cadc1ff9d08d6ee8afa8064aa0a
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-08-15 19:40:18 +0100

    Fix typo error.

--- src/com/mounacheikhna/algorithms/MaxSubArray.py
+++ src/com/mounacheikhna/algorithms/MaxSubArray.py
@@ -3,6 +3,6 @@
 def max_subarray(A):
     max_ending_here = max_so_far = A[0]
     for x in A[1:]:
-        max_ending_here = max(x, max_ending_here + x)
+        max_ending_here = max(0, max_ending_here + x)
         max_so_far = max(max_so_far, max_ending_here)
     return max_so_far
