b5071afe88716b6bdf038235681078f093a26a94
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-12-13 08:19:45 +0000

    Add test cases.

--- src/com/mounacheikhna/algorithms/array/MaxSubarray.py
+++ src/com/mounacheikhna/algorithms/array/MaxSubarray.py
@@ -1,6 +1,16 @@
+
+import random
+
+def generate_array(item_count, lower_bound, upper_bound):
+    number_list = []
+    for x in range(1, item_count):
+        number_list.append(random.randint(lower_bound, upper_bound))
+    return number_list
+
+
 # using divide and conquer
 def max_subarray_divide_and_conquer(arr):
-    helper_max_subarray_div_conquer(arr, 0, arr.length - 1)
+    helper_max_subarray_div_conquer(arr, 0, (len(arr) - 1))
 
 
 def helper_max_subarray_div_conquer(arr, l, r):
@@ -19,13 +29,23 @@ def helper_max_subarray_div_conquer(arr, l, r):
         sum += arr[i]
         rmax = max(sum, rmax)
 
-    return max(lmax + rmax, helper_max_subarray_div_conquer(l, m), helper_max_subarray_div_conquer(m + 1, r))
+    return max(lmax + rmax, helper_max_subarray_div_conquer(arr, l, m), helper_max_subarray_div_conquer(arr, m + 1, r))
 
 
 # using kadane algorithm
-def max_subarray(arr):
+def max_subarray_kadane(arr):
     max_so_far = max_ending_here = 0
     for x in arr:
         max_ending_here = max(0, max_ending_here + x)
         max_so_far = max(max_so_far, max_ending_here)
     return max_so_far
+
+
+magnitude = input('enter vector size: ')
+number_list = generate_array(magnitude, -10, 10)
+
+print "divide and conquer:"
+print max_subarray_divide_and_conquer(number_list)
+
+print "kadane:"
+print max_subarray_kadane(number_list)
