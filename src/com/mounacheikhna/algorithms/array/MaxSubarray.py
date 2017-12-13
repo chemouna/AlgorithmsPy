e4e9340f420ccc1b3fc2840fd0272d53bb68a5fb
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-12-13 07:58:18 +0000

    Max subarray.

---
+++ src/com/mounacheikhna/algorithms/array/MaxSubarray.py
@@ -0,0 +1,6 @@
+def max_subarray(arr):
+    max_so_far = max_ending_here = 0
+    for x in arr:
+        max_ending_here = max(0, max_ending_here + x)
+        max_so_far = max(max_so_far, max_ending_here)
+    return max_so_far
